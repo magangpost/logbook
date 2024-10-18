2a6432b142b72202063c96404721607c62bdd9c0
Ibrohim Mubarok <befousmain@gmail.com>
2024-10-18 10:03:40 +0700

    Buat agar tidak dapat lacak jika kodepelanggan berbeda

--- app/Http/Controllers/LacakController.php
+++ app/Http/Controllers/LacakController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Transaksi;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class LacakController extends Controller
 {
@@ -15,11 +16,16 @@ class LacakController extends Controller
 
     public function show(Request $request)
     {
+        $kodepelanggan = Auth::user()->kodepelanggan;
         $no_resi = $request->query('no_resi');
         $transaksi = Transaksi::where('no_resi', $no_resi)->first();
         
         if (!$transaksi) {
             return redirect()->route('lacak.index')->with('error', 'Transaction not found.');
+        }
+
+        if ($transaksi->kodepelanggan !== $kodepelanggan) {
+            return redirect()->route('lacak.index')->with('error', 'Unauthorized access to transaction data.');
         }
         
         $transaksi->lacak = collect($transaksi->lacak)->sortByDesc('id_lacak')->values()->toArray();
