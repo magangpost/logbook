0af38639ac80bd291d3ab91722efd49c86d54c26
Ibrohim Mubarok <befousmain@gmail.com>
2024-10-22 11:37:13 +0700

    Benerin input password

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -51,14 +51,8 @@
                             <label for="password" class="col-md-5 col-form-label text-md-start">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control " name="password" required autocomplete="new-password">
                                 <small class="form-text text-danger">Kosongkan jika tidak ingin mengganti password</small>
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
